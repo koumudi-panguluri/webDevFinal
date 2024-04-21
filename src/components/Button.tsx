7281a9aace701459d60b2597465ed17cec1a1597
Koumudi Panguluri <pangulurikoumudi@gmail.com>
2024-04-21 11:47:33 -0400

    minor change

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link'
+
 import clsx from 'clsx'
+import Link from 'next/link'
 
 const variantStyles = {
   primary:
@@ -8,18 +9,18 @@ const variantStyles = {
     'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
 }
 
-type ButtonProps = {
-  variant?: keyof typeof variantStyles
-} & (
-  | (React.ComponentPropsWithoutRef<'button'> & { href?: undefined })
-  | React.ComponentPropsWithoutRef<typeof Link>
-)
+// type ButtonProps = {
+//   variant?: keyof typeof variantStyles
+// } & (
+//   | (React.ComponentPropsWithoutRef<'button'> & { href?: undefined })
+//   | React.ComponentPropsWithoutRef<typeof Link>
+// )
 
 export function Button({
   variant = 'primary',
   className,
   ...props
-}: ButtonProps) {
+}) {
   className = clsx(
     'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
     variantStyles[variant],
@@ -27,8 +28,10 @@ export function Button({
   )
 
   return typeof props.href === 'undefined' ? (
-    <button className={className} {...props} />
+    <>
+      <button className={className} {...props} />
+    </>
   ) : (
-    <Link className={className} {...props} />
+    <Link href="#" className={className} {...props} />
   )
 }
